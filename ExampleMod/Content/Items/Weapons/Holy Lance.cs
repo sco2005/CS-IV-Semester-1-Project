9f5149e09b61c25c382078462f7f6e1dc23f2625
sco2005 <thiagoscocat2005@gmail.com>
2022-10-10 12:34:16 -0500

    Update
    Adds more weapons, some preliminary crafting setups

--- ExampleMod/Content/Items/Weapons/Holy Lance.cs
+++ ExampleMod/Content/Items/Weapons/Holy Lance.cs
@@ -17,27 +17,27 @@ namespace ExampleMod.Content.Items.Weapons
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 		}
 
-		public override void SetDefaults() {
+		public override void SetDefaults() {h
 			// Common Properties
 			Item.rare = ItemRarityID.Pink; // Assign this item a rarity level of Pink
 			Item.value = Item.sellPrice(silver: 10); // The number and type of coins item can be sold for to an NPC
 
 			// Use Properties
 			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
-			Item.useAnimation = 12; // The length of the item's use animation in ticks (60 ticks == 1 second.)
-			Item.useTime = 18; // The length of the item's use time in ticks (60 ticks == 1 second.)
+			Item.useAnimation = 10; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			Item.useTime = 10; // The length of the item's use time in ticks (60 ticks == 1 second.)
 			Item.UseSound = SoundID.Item71; // The sound that this item plays when used.
 			Item.autoReuse = true; // Allows the player to hold click to automatically use the item again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
 
 			// Weapon Properties
-			Item.damage = 25;
-			Item.knockBack = 6.5f;
+			Item.damage = 150;
+			Item.knockBack = 3f;
 			Item.noUseGraphic = true; // When true, the item's sprite will not be visible while the item is in use. This is true because the spear projectile is what's shown so we do not want to show the spear sprite as well.
 			Item.DamageType = DamageClass.Melee;
 			Item.noMelee = true; // Allows the item's animation to do damage. This is important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
 
 			// Projectile Properties
-			Item.shootSpeed = 3.7f; // The speed of the projectile measured in pixels per frame.
+			Item.shootSpeed = 7f; // The speed of the projectile measured in pixels per frame.
 			Item.shoot = ModContent.ProjectileType<ExampleSpearProjectile>(); // The projectile that is fired from this weapon
 		}
 
