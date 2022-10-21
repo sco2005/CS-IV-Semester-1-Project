7324ce1f017dc943ac0a24ea1c3f143f189d1e00
sco2005 <111914842+sco2005@users.noreply.github.com>
2022-10-21 00:23:09 -0500

    fixes aaaaaaaa (needs more fixes)

--- ExampleMod/Content/Items/Weapons/ExampleSpear.cs
+++ ExampleMod/Content/Items/Weapons/ExampleSpear.cs
@@ -24,14 +24,14 @@ namespace ExampleMod.Content.Items.Weapons
 
 			// Use Properties
 			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
-			Item.useAnimation = 12; // The length of the item's use animation in ticks (60 ticks == 1 second.)
-			Item.useTime = 18; // The length of the item's use time in ticks (60 ticks == 1 second.)
+			Item.useAnimation = 8; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			Item.useTime = 1; // The length of the item's use time in ticks (60 ticks == 1 second.)
 			Item.UseSound = SoundID.Item71; // The sound that this item plays when used.
 			Item.autoReuse = true; // Allows the player to hold click to automatically use the item again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
 
 			// Weapon Properties
-			Item.damage = 25;
-			Item.knockBack = 6.5f;
+			Item.damage = 35;
+			Item.knockBack = 0.5f;
 			Item.noUseGraphic = true; // When true, the item's sprite will not be visible while the item is in use. This is true because the spear projectile is what's shown so we do not want to show the spear sprite as well.
 			Item.DamageType = DamageClass.Melee;
 			Item.noMelee = true; // Allows the item's animation to do damage. This is important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
